4894cd8fdb890a983f6d09df184b206e6eadc750
Melvin jerry <+>
2024-11-07 14:48:00 +0300

    bugs

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,25 +1,42 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    def __init__(self,brand,size) -> None:
-        self.brand=brand
-        self.size=size
-      
+    def __init__(self, brand, size) -> None:
+        self.brand = brand
+        self.size = size
+        self._condition = 'Old'  # Initialize condition attribute
 
+    # Property getter and setter for size
     @property
     def size(self):
         return self._size
     
     @size.setter
-    def size(self,size):
-        if isinstance(size,int):
-            self._size=size
+    def size(self, size):
+        if isinstance(size, int):
+            self._size = size
         else:
             print("size must be an integer")
     
+    # Method to simulate cobbling (repairing) the shoe
     def cobble(self):
         print("Your shoe is as good as new!")
+        self._condition = "New"  # After cobbling, the shoe's condition is 'New'
 
+    # Property for condition to access it
+    @property
     def condition(self):
-         self.condition == "New"
-        
+        return self._condition
+    
+    @condition.setter
+    def condition(self, value):
+        if value in ["New", "Old"]:
+            self._condition = value
+        else:
+            print("Invalid condition!")
+
+# Test to check the cobble method
+shoe = Shoe(brand="Nike", size=42)
+print(shoe.condition)  # Should print 'Old'
+shoe.cobble()
+print(shoe.condition)  # Should print 'New'
